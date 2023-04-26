4efd125fb51247a5b55e14fde5f150b382e23f8c
tutorial <ishan.gaba@simplilearn.net>
2023-04-26 17:41:10 +0100

    Modify client and server code for task 2

--- ipc-server.py
+++ ipc-server.py
@@ -3,7 +3,7 @@
 
 import socket
 
-HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 9898        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
--- server/ipc-server.py
+++ server/ipc-server.py
@@ -2,6 +2,7 @@
 # ipc_server.py
 
 import socket
+import statistics
 
 HOST = socket.gethostbyname('ipc_server_dns_name')
 PORT = 9898        # Port to listen on (non-privileged ports are > 1023)
@@ -12,9 +13,23 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn, addr = s.accept()
 
     with conn:
-        print('Connected by', addr)
+        # print('Connected by', addr)
+        received = []
         while True:
             data = conn.recv(1024)
             if not data:
                 break
-            conn.sendall(data)
+            received.append(data.decode())
+            # conn.sendall(data)
+
+        # once all the data has been received from client
+        avg = statistics.mean(received)
+        med = statistics.median(received)
+        sd = statistics.stdev(received)
+
+        # encode to binary to send to client
+        conn.sendall(str(avg).encode())
+        conn.sendall(str(med).encode())
+        conn.sendall(str(sd).encode())
+        print("Sent " + "Average: " + str(avg) +
+              ", Median: " + str(med) + ", Stdev: " + str(sd))
